4759d2cebff3644c758688647617c39f82c5790f
Johannes Elschner <johanneselschner@gmail.com>
2022-06-29 21:57:54 +0200

    refactor: Order imports alphabetically
    Order imports datetime, numpy, pandas and time alphabetically
    to bring in some structure

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
-import time
-import pandas as pd
-import numpy as np
 import datetime as dt
+import numpy as np
+import pandas as pd
+import time
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
